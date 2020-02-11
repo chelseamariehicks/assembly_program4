c5634172df3c0473ae5ae5966bf746b20e17d556
Chelsea Marie Hicks <chelseamariehicks@gmail.com>
2020-02-11 10:39:55 -0800

    Update prog04.asm

--- prog04.asm
+++ prog04.asm
@@ -12,7 +12,7 @@ TITLE Programming Assignment #4     (prog04.asm)
 ;		integer greater than one is either composite or prime; one is not a
 ;		composite.
 ;
-;		This program implements procedures, loops, and utrilizes 
+;		This program implements procedures, loops, and utilizes 
 ;		data validation to verify that the user entered a number
 ;		within the range of [1...400] and repeatedly prompts user to enter
 ;		a number within the range until they do so.
@@ -35,14 +35,19 @@ numPrompt		BYTE	"Enter the number of composites to display [1...400]: ", 0
 errMsg			BYTE	"The number you entered is out of range! Try again.", 0
 
 checkInput		DWORD	?				;temp variable to store user input to be checked
-isValid			DWORD	?				;variable to store confirmation of valid number
+isValid			DWORD	0				;variable to store confirmation of valid number
 userInput		DWORD	?				;variable to store valid user input
 
 ;messages printed to screen to display composites and necessary variables 
 resultsMsg		BYTE	" composite numbers on display:", 0
 space			BYTE	"   ", 0
 
-count			DWORD	0				;variable to count composites printed on a line						
+lineCount		DWORD	0				;variable to count composites printed on a line	
+numsPrinted		DWORD	0				;variable to count numbers printed
+currentNum		DWORD	4				;variable to track the current number being checked
+compositeFlag	DWORD	0				;variable to store confirmation of composite
+
+divisor			DWORD	?				;variable for the divisor in isComposite
 
 ;message printed to screen for farewell
 goodbye			BYTE	"Farewell my mathematical friend. Results certified by Chelsea.", 0
@@ -119,7 +124,7 @@ invalidInput:
 	jmp		dataLoop
 
 validInput:
-	mov		eax, checkInput
+	mov		eax, checkInput					;user input is valid
 	mov		userInput, eax
 	call	Crlf
 	ret
@@ -133,12 +138,12 @@ getUserData ENDP
 ; Returns: none
 ; Registers changed: edx
 ;-----------------------------------------------------------------------
-validate PROC 
-	cmp		checkInput, UPPER_LIMIT
+validate PROC USES edx
+	cmp		checkInput, UPPER_LIMIT			;check that user input is not greater than 400
 	jg		validationFail
-	cmp		checkInput, LOWER_LIMIT
+	cmp		checkInput, LOWER_LIMIT			;check that user input is not less than 1 
 	jl		validationFail
-	mov		isValid, 1
+	mov		isValid, 1						;change isValid to 1 to represent true, valid number
 	jmp		validationComplete
 
 validationFail:
@@ -152,20 +157,157 @@ validate ENDP
 ;-----------------------------------------------------------------------
 ; showComposites
 ;
-; Procedure to calculate and display composite numbers
+; Procedure to display composite numbers, calls isComposite to calculate
 ; Receives: none
 ; Returns: none
-; Registers changed: edx, eax, ebx
+; Registers changed: edx, eax, ecx
 ;-----------------------------------------------------------------------
-showComposites PROC
-	mov		edx, OFFSET userInput
-	call	WriteInt
+showComposites PROC USES edx eax ecx
+	mov		eax, userInput					;print message for numbers displaying
+	call	WriteDec
 	mov		edx, OFFSET resultsMsg
 	call	WriteString
 	call	Crlf
+	call	Crlf
 
+	mov		ecx, userInput					;loop counter equal to valid userInput
+
+displayStart:
+	call	isComposite
+	cmp		compositeFlag, 1				;check if currentNum is composite using flag
+	je		compositeValid					;if currentNum iscomposite, jump to compositeValid
+	inc		currentNum						;if not composite, check next number
+	jmp		displayStart
+
+compositeValid:
+	cmp		lineCount, 10					;check if lineCount is 10
+	jne		displayComposite
+	call	Crlf							;skip to next line and update lineCount
+	mov		lineCount, 0
+
+displayComposite:
+	inc		lineCount						
+	mov		eax, currentNum					;print current composite number
+	call	WriteDec
+	mov		edx, OFFSET space
+	call	WriteString
+	inc		numsPrinted						;increase numPrinted
+	inc		currentNum						;increase currentNum
+	loop	displayStart
 	ret
 showComposites ENDP
+
+;-----------------------------------------------------------------------
+; isComposite
+;
+; Procedure to determine if a number is composite or not
+; Receives: currentNum
+; Returns: compositeFlag
+; Registers changed: edx, eax
+;-----------------------------------------------------------------------
+isComposite PROC USES edx eax
+checkTwo:
+	mov		divisor, 2						;check if currentNum can be divided by 2
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkThree
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkThree:
+	mov		divisor, 3						;check if currentNum can be divided by 3
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkFive
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkFive:
+	cmp		currentNum, 5
+	je		compositeFailed
+	mov		divisor, 5						;check if currentNum can be divided by 5
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkSeven
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkSeven:
+	cmp		currentNum, 7
+	je		compositeFailed
+	mov		divisor, 7						;check if currentNum can be divided by 7
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkEleven
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkEleven:
+	cmp		currentNum, 11
+	je		compositeFailed
+	mov		divisor, 11						;check if currentNum can be divided by 11
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkThirteen
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkThirteen:
+	cmp		currentNum, 13
+	je		compositeFailed
+	mov		divisor, 13						;check if currentNum can be divided by 13
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkSeventeen
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkSeventeen:
+	cmp		currentNum, 17
+	je		compositeFailed
+	mov		divisor, 17						;check if currentNum can be divided by 17
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkNineteen
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkNineteen:
+	cmp		currentNum, 19
+	je		compositeFailed
+	mov		divisor, 19						;check if currentNum can be divided by 19
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		checkTwentyThree
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+checkTwentyThree:
+	cmp		currentNum, 23
+	je		compositeFailed
+	mov		divisor, 23						;check if currentNum can be divided by 23
+	mov		edx, 0
+	mov		eax, currentNum
+	div		divisor
+	cmp		edx, 0							;if remainder is 0, it is composite
+	jne		compositeFailed
+	mov		compositeFlag, 1
+	jmp		composCheckComplete
+
+compositeFailed:
+	mov		compositeFlag, 0
+	
+composCheckComplete:
+	ret
+isComposite ENDP
 
 
 ;-----------------------------------------------------------------------
@@ -177,6 +319,8 @@ showComposites ENDP
 ; Registers changed: edx
 ;-----------------------------------------------------------------------
 farewell PROC USES edx
+	call	Crlf
+	call	Crlf
 	mov		edx, OFFSET goodbye
 	call	WriteString
 	call	Crlf
